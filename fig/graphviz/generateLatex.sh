c46ada50ce25d68229b8e822f0a14b4217c4e346
Lukas Retschmeier <lukas.retschmeier@tum.de>
2022-12-21 15:10:34 +0100

    More embedding

--- fig/graphviz/generateLatex.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# assiumes dot2tex was installed.
-dot2tex -f tikz graphclasses.dot -o test.tikz
---
+++ fig/graphviz/genfig.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Then remove the 
+dot2tex -f tikz graphclasses.dot --figonly -o graphclasses.tikz
